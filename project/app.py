562f120d1a95b2e90d2cf1a07719a4403d05f319
Oliver Becher <becheo@gmail.com>
2020-03-31 16:07:50 +0200

    Add new table design to dashboard site

--- project/app.py
+++ project/app.py
@@ -245,33 +245,49 @@ def logout():
 @app.route('/dashboard')
 @is_logged_in
 def dashboard():
+
+    # get data from database
     cur = mysql.connection.cursor()  # creates database cursor
-
-    # Get file entries from database
-    # TODO select all from files wher user is session['username']
-    result = cur.execute("SELECT * FROM files")
+    cur.execute("SELECT * FROM files WHERE username = %s",
+                [session['username']])
     files = cur.fetchall()  # fetches in dictionary form
+    cur.close()
 
-    voltage_curve = 'USB6009-Messung.png'  # TODO ID o.ä übergeben
-
-    # für plot: IDs die aktuell in DB sind anzeigen lassen
-    # ID an render_template übergeben damit die richtigen plots angezeigt werden können
-
-    # remove id from filename for presentation on dashboard
+    # get data for files
     for i in range(len(files)):
+        # user voltage specifications
+        path = apph.UPLOAD_FOLDER + '/' + files[i]['name']
+        data = apph.read_txt_by_lines(path)
+        xdata = list(range(len(data)))
+        xdata = [(x/apph.samplerate_write) for x in xdata]
+        files[i]['xdata'] = xdata
+        data = [float(j) for j in data]
+        files[i]['ydata'] = data
+
+        # result files with data from measurement
+        if files[i]['status'] == 'executed':
+            path = apph.RESULTS_FOLDER + '/' + 'results-' + files[i]['name']
+            data = apph.read_results_by_lines(path)
+            files[i]['xdata_results'] = data[1]  # time
+            files[i]['ydata_results_mot_volt'] = data[4]
+            files[i]['ydata_results_gen_volt'] = data[3]
+            files[i]['ydata_results_rpm'] = data[2]
+            files[i]['ydata_results_current'] = data[5]
+            files[i]['ydata_results_temp'] = data[6]
+            files[i]['temp_min_value'] = min(data[6]) - 5
+            files[i]['temp_max_value'] = max(data[6]) + 5
+
+            path = '../static/results/' + 'results-' + files[i]['name']
+            files[i]['result_path'] = path  # for download hyperlink
+
+        # remove id from filename for presentation on dashboard
         name = files[i]['name']
         name = name.split('-', 1)[1]  # split(seperator, maxsplit)[element]
         files[i]['name'] = name
+        files[i]['index'] = str(i)
 
-    # for i in range(len(files)):
-    #     print(files[i])
-
-    # data to show in dashboard (plotly graph)
-    xdata = [1, 2, 3, 4, 5]
-    ydata = [1, 2, 10, 12, 16]
-
-    if result > 0:  # if there are rows
-        return render_template('dashboard.html', files=files, Spannungsverlauf=voltage_curve, xdata=xdata, ydata=ydata)
+    if len(files) > 0:  # if there are rows
+        return render_template('dashboard.html', files=files)
 
     else:
         msg = 'Keine Dateien vorhanden'
