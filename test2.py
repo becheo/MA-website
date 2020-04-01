ad589523742564f41fc7e881173d190486f022ad
Oliver Becher <becheo@gmail.com>
2020-04-02 00:01:28 +0200

    Change data loading for dashboard to JavaScript
    Compared to loading the data from the uploaded and result files with
    python the usage of JavaScript offers some advantages:
    
    - better user experience, because site is rendered much faster
      (especially with long datsets)
    
    - files are saved in browser cache so that they don't have to be
      reloaded during a session
    
    - generally feels faster

--- test2.py
+++ test2.py
@@ -10,7 +10,19 @@ import time
 # data = list(range(len(number)))
 # print(data)
 
-if 'files' in locals():
-    print("Dateien vorhande")
-else:
-    print("keine Dateien vorhanden")
+# if 'files' in locals():
+#     print("Dateien vorhande")
+# else:
+#     print("keine Dateien vorhanden")
+data = [0, 1, 2, 3, 3, 3, 3, 3, 3, 3, 3, 2, 2, 2, 2, 1, 1, 0]
+
+with open("USB6009-Messung.csv", "w") as f:
+    f.write("Header")
+    f.write("Spalteninhalt")
+
+    for i in range(0, len(data), 1):
+        f.write("{}" .format(data[i]))
+        f.write(",{:.4f}" .format(data[i]))
+        f.write(",{:.4f}" .format(data[i]))
+        f.write("\n")
+    f.close()
