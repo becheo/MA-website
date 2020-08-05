8a33321503ae5aedab2a0dde870b2300d3c444e1
Oliver Becher <becheo@gmail.com>
2020-08-05 15:24:14 +0200

    Code cleanup

--- project/app.py
+++ project/app.py
@@ -724,9 +724,6 @@ def generate(camera):
 @is_logged_in
 def webcam():
 
-    # import cv2
-    # import numpy as np
-
     # get files in queue to show which test is currently running
     cur = mysql.connection.cursor()
     cur.execute("SELECT * FROM queue")
